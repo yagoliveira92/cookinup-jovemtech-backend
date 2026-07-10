e4e5ad909f5d90517aa6480c4a5b96f643e02f17
Yago Souza Oliveira <yagoliveira92@gmail.com>
2026-07-09 21:08:54 -0300

    Add Prisma client and implement categorias endpoint

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 import express from 'express'
 import cors from 'cors'
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req,res) => {
     res.send('API do CookInUp funcionando!');
+});
+
+app.get('/categorias', async (req, res) => {
+    try {
+        const categorias = await prisma.categoria.findMany({
+            include: {
+                ingredientes: true
+            }
+        });
+        res.json(categorias);
+    } catch (error) {
+        res.status(500).json({ erro: "Erro ao buscar dados do banco."});
+    }
 });
 
 app.listen(3000, () => {
