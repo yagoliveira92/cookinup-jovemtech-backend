25a7a08cc5025bf02704cd8d73bae8bc63cdd8e4
Yago Souza Oliveira <yagoliveira92@gmail.com>
2026-07-10 21:11:52 -0300

    Refactor categorias endpoint for improved readability and formatting

--- server.js
+++ server.js
@@ -7,20 +7,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('API do CookInUp funcionando!');
 });
 
 app.get('/categorias', async (req, res) => {
+
+    const { busca } = req.query;
+    
     try {
         const categorias = await prisma.categoria.findMany({
+            where: busca ? {
+                nome: {
+                    contains: busca
+                }
+            } : {},
             include: {
                 ingredientes: true
             }
         });
         res.json(categorias);
     } catch (error) {
-        res.status(500).json({ erro: "Erro ao buscar dados do banco."});
+        res.status(500).json({ erro: "Erro ao buscar dados do banco." });
     }
 });
 
